22c5ee8e0232f06b96e6e1c08a5ed22a846b035a
Joseph Collier <jcollier joe.j.col@gmail.com>
2018-06-24 19:41:34 +0000

    Updating fillable fields in ViewsModel

--- app/Models/Views.php
+++ app/Models/Views.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Views extends Model
 {
-    //
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'url',
+        'ip_address',
+        'session_id',
+        'item_type',
+        'item_id',
+        'email',
+        'marketing_tracking_code',
+    ];
 }
